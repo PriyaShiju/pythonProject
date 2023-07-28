72e81d3e251e32a637ec14f0244e25c97f74217e
PriyaShiju <83154721+PriyaShiju@users.noreply.github.com>
2023-07-28 13:05:56 +1000

    Update settings.py

--- contactformproj/contactformproj/settings.py
+++ contactformproj/contactformproj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hl08o=*j#9c(57sm(z4p43g(rr_xi-vzqxfqrj07z2&740-jii'
+SECRET_KEY = '***********************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
